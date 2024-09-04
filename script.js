07dbd103bb8985a31661438dfc335d4ca9fa0550
Hemanthmon <153289923+Hemanthmon@users.noreply.github.com>
2024-09-04 06:24:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+//your JS code here. If required.let
+let smallWord = getElementId('fname');
+
+for(i=0; i <= smallWord.length-1; i++){
+let bigWord = smallWord.Captalize;
+}
